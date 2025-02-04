040398f92219caf1688ca71f9db515d6d6cf4e23
Dan Rascop <80607356+drascop@users.noreply.github.com>
2025-02-03 18:49:37 -0600

    Revert "removed redundant getters and setters of subclasses. fix constructor in Item"
    This reverts commit 5d0669598c94dcf9a7ed1b298dad9b754c62a14f.

--- MMAD/MMAD/src/main/java/com/MMAD/MMAD/model/Item.java
+++ MMAD/MMAD/src/main/java/com/MMAD/MMAD/model/Item.java
@@ -1,83 +1,61 @@
 package com.MMAD.MMAD.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 
-@MappedSuperclass
+@Entity
 public abstract class Item {
-
     @Column(nullable = false, updatable = false)
     protected String sourceID;
-
     protected String name;
-
     protected String imageURL;
-
     @Id
     @Column(nullable = false, updatable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     protected int id;
 
-
-    /**
-     * Empty constructor for JPA.
-     */
     public Item(){}
 
-
-    
-    /**
-     * Constructor taking all parameters. id is generated automatically.
-     * 
-     * @param sourceID The item's source ID (spotify).
-     * @param name The item's name.
-     */
     public Item(String sourceID, String name) {
         this.sourceID = sourceID;
+        this.id = -1;
         this.name = name;
     }
-
 
     public String getImageURL() {
         return imageURL;
     }
 
-
     public void setImageURL(String imageURL) {
         this.imageURL = imageURL;
     }
-
 
     public int getId() {
         return id;
     }
 
-
     public void setId(int id) {
         this.id = id;
     }
-
 
     public String getSourceID() {
         return sourceID;
     }
 
-
     public void setSourceID(String sourceID) {
         this.sourceID = sourceID;
     }
-
 
     public String getName() {
         return name;
     }
 
-
     public void setName(String name) {
         this.name = name;
     }
 
+    public Item(int id, String sourceID, String name) {
+        this.id = id;
+        this.sourceID = sourceID;
+        this.name = name;
+    }
 }
--- MMAD/MMAD/src/main/java/com/MMAD/MMAD/model/Song.java
+++ MMAD/MMAD/src/main/java/com/MMAD/MMAD/model/Song.java
@@ -1,45 +1,39 @@
 package com.MMAD.MMAD.model;
-
-import jakarta.persistence.Entity;
-
-@Entity
 public class Song extends Item{
-
     private Album album;
     private Artist artist;
 
-
-    /**
-     * Constructor taking all parameters.
-     * 
-     * @param sourceID The song's source ID (spotify).
-     * @param name The song's name.
-     * @param artist The song's artist.
-     * @param album The song's album.
-     */
     public Song(String sourceID, String name, Artist artist, Album album){
         super(sourceID, name);
         this.artist = artist;
         this.album = album;
     }
-
-    
-    /**
-     * Constructor taking all parameters except for album. Used for singles.
-     *
-     * @param sourceID The song's source ID (spotify).
-     * @param name The song's name.
-     * @param artist The song's artist.
-     */
-    public Song(String sourceID, String name, Artist artist){
-        super(sourceID, name);
+    public Song(int id,String sourceID, String name, Artist artist, Album album){
+        super(id, sourceID, name);
         this.artist = artist;
+        this.album = album;
+    }
+
+    public void setID(int id){
+        this.id = id;
+    }
+
+    @Override
+    public String getName(){
+        return this.name;
+    }
+    @Override
+    public String getSourceID(){
+        return this.sourceID;
+    }
+    @Override
+    public int getID(){
+        return this.id;
     }
 
     public Album getAlbum(){
         return this.album;
     }
-
 
     public Artist getArtist(){
         return this.artist;
