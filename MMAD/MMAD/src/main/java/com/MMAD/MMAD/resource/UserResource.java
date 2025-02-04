cae0f8b7794bffcc68503c01b870855e632ad689
Dan Rascop <80607356+drascop@users.noreply.github.com>
2025-02-04 17:07:39 -0600

    minor work on UserResource

--- MMAD/MMAD/src/main/java/com/MMAD/MMAD/resource/UserResource.java
+++ MMAD/MMAD/src/main/java/com/MMAD/MMAD/resource/UserResource.java
@@ -3,8 +3,15 @@ package com.MMAD.MMAD.resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.MMAD.MMAD.service.UserService;
+
+
 @RestController
 @RequestMapping("/user")
 public class UserResource {
-    
+    private final UserService userService;
+
+    public UserResource(UserService userService){
+        this.userService = userService;
+    }
 }
