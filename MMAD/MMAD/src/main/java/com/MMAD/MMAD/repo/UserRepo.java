eed0d2f9a55c063f812dde9d61e92004d5928d38
Dan Rascop <80607356+drascop@users.noreply.github.com>
2025-02-04 16:53:19 -0600

    start work on user repo

--- MMAD/MMAD/src/main/java/com/MMAD/MMAD/repo/UserRepo.java
+++ MMAD/MMAD/src/main/java/com/MMAD/MMAD/repo/UserRepo.java
@@ -1,9 +1,14 @@
 package com.MMAD.MMAD.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.MMAD.MMAD.model.User;
 
 public interface UserRepo extends JpaRepository<User, Long> {
-
+    Optional<User> findUserByUsername(String username);
+    Optional<User> findUserById(Long id);
+    void deleteUserById(Long id);
+    Optional<User> findUserByUsernameAndPassword(String username, String password);
 }
