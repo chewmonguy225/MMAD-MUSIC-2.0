894d34bf01c87633ad8db1a272e1871b9c32ab51
magust4 <149904845+magust4@users.noreply.github.com>
2025-02-17 19:54:07 -0600

    album

--- MMAD/frontend/src/app/model/album.type.ts
+++ MMAD/frontend/src/app/model/album.type.ts
@@ -1,9 +1,10 @@
 import { Item } from './item.type'
 
-export class Artist extends Item{
+export class Album extends Item{
     constructor(
         id: number = 0,
         sourceID: String = "",
+        artistID: number = 0,
         name: String,
         imageURL: String = "default.jpg") {
         super(id, sourceID, name, imageURL);
